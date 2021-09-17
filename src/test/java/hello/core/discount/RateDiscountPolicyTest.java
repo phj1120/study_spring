a29bf252088457403d59b48234c8667649df685a
phj1120 <hyeonj1998@naver.com>
2021-09-17 18:40:30 +0900

    새로운 할인 정책 적용과 문제점

---
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,36 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.order.Order;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP : 10% 할인")
+    void vip_o(){
+        //given
+        Member member = new Member(1L, "hj", Grade.VIP);
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+        //then
+        Assertions.assertThat(discount).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("VIP 아닌 경우")
+    void vip_x(){
+        //given
+        Member member = new Member(2L, "phj", Grade.BASIC);
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+        //then
+        Assertions.assertThat(discount).isEqualTo(0);
+    }
+}
