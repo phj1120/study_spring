bb3da018ecc37229579188434458484f6b2ebd8b
phj1120 <hyeonj1998@naver.com>
2021-09-27 19:00:06 +0900

    개인공부 : 할인된 가격 출력해 보기

--- src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -1,6 +1,8 @@
 package hello.core.scan;
 
 import hello.core.AutoAppConfig;
+import hello.core.member.Grade;
+import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.order.OrderServiceImpl;
@@ -11,14 +13,19 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class AutoAppConfigTest {
 
     @Test
-    void basicScan(){
+    void basicScan() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
 
         MemberService memberService = ac.getBean(MemberService.class);
         Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
 
-        OrderServiceImpl bean = ac.getBean(OrderServiceImpl.class);
-        MemberRepository memberRepository = bean.getMemberRepository();
+        OrderServiceImpl orderService = ac.getBean(OrderServiceImpl.class);
+
+        MemberRepository memberRepository = orderService.getMemberRepository();
         System.out.println("memberRepository = " + memberRepository);
+
+        Member member = new Member(1L, "A", Grade.VIP);
+        memberService.join(member);
+        System.out.println("discount price = " + orderService.createOrder(1L, "itemA", 20000).calculatePrice());
     }
 }
