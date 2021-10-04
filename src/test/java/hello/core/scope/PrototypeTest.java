1bc42d4dcf979e370ec3863e7fd4a84db572d3f9
phj1120 <hyeonj1998@naver.com>
2021-10-04 23:13:00 +0900

    빈스코프, 프로토타입 스코프 설명

--- src/test/java/hello/core/scope/PrototypeTest.java
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -20,6 +20,7 @@ public class PrototypeTest {
 
         System.out.println("find PrototypeBean2");
         PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+
         System.out.println("prototypeBean1 = " + prototypeBean1);
         System.out.println("prototypeBean2 = " + prototypeBean2);
         assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
@@ -28,7 +29,35 @@ public class PrototypeTest {
         prototypeBean2.destroy();
         ac.close();
     }
+/*
+find prototypeBean1
+PrototypeBean.init
+find PrototypeBean2
+PrototypeBean.init
+prototypeBean1 = hello.core.scope.PrototypeTest$PrototypeBean@6ee6f53
+prototypeBean2 = hello.core.scope.PrototypeTest$PrototypeBean@421bba99
+PrototypeBean.destroy
+PrototypeBean.destroy
 
+이렇게 출력됨
+스프링 빈 조회 후
+find ~ bean1 출력
+ac.getBean으로 조회 init 호출
+find ~ bean2 출력
+ac.getBean으로 조회 init 호출
+soutv bean1
+soutv bean2
+수동으로 종료
+수동으로 종료
+
+
+요약하면
+싱글톤 스코프 빈은 스프링 컨테이너 생성시 초기화 메소드 실행, 스프링 컨테이너 종료시 종료 메소드 실행
+프로토타입 스코프 빈은 스프링 컨테이너에서 빈을 조회할 때 생성, 초기화 메서드 실행, 스프링 컨테이너 종료되도 유지, 직접 종료해야 종료
+    - 스프링 컨테이너 요청하때마다 생성
+    - 빈의 생성과 의존관계 주입, 초기화 까지만 관여
+
+ */
     @Scope("prototype")
 //    @Component 생략 가능 -> AnnotationConfigApplicationContext에 지정해주면 컴포넌트 스캔처럼 동작함
     static class PrototypeBean{
