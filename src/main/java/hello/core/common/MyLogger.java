a3e19afc0443be0e19a2ea83f042b7f8e26f94f1
phj1120 <hyeonj1998@naver.com>
2021-10-05 21:35:53 +0900

    스코프와 프록시

--- src/main/java/hello/core/common/MyLogger.java
+++ src/main/java/hello/core/common/MyLogger.java
@@ -1,6 +1,7 @@
 package hello.core.common;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -8,7 +9,17 @@ import javax.annotation.PreDestroy;
 import java.util.UUID;
 
 @Component
-@Scope(value = "request")
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
+// 껍데기 MyLogger 를 넣고 실제 동작할 때 진짜 MyLogger 를 넣음
+// class hello.core.common.MyLogger$$EnhancerBySpringCGLIB$$cf8658ee
+// ac.getBean("myLogger", MyLogger.class) 해도 프록시 객체가 조회됨
+// 실제 요청이 와야지만 생성되고 진짜를 찾아옴
+// 그래서 프록시라고 함
+// 가짜 프록시 개체는 request scope 와 관계가 없음 그냥 가짜고 내부에 단순한 위임 로직만 있고 싱글톤 처럼 동작
+// 싱글톤 쓰듯이 request scope 사용 가능
+// 뭘 쓰든 진짜 객체 조회를 꼭 필요한 시점 가지 지연처리 한다는 점
+// 다형성과 DI 컨테이너가 가진 큰 강점임
+// 싱글톤이랑 비슷하게 사용하지만 동작은 확 다르기 때문에 꼭 필요한 경우에만 사용 할 것
 public class MyLogger {
 
     private String uuid;
--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -11,33 +11,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
-//@RequiredArgsConstructor
+@RequiredArgsConstructor
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
-//    private final MyLogger myLogger;
-    /*
-    스프링 컨테이너에 등록되고 받아오려 하는데 값이 없음
-    얘는 생존 기간이 고객의 요청이 들어와서 나갈때 까지인데
-    리퀘스트가 없으니까 당연히 내용이 없지 그래서 오류남
-    Scope 'request' is not active
-
-    Provider 를 여기서 쓰면 됩니다.
-        ? 음.... 싱글톤에서 다 반납하기 위한게 Provider 아니었나..
-    */
-
-    @Autowired
-    public LogDemoController(LogDemoService logDemoService, ObjectProvider<MyLogger> myLoggerProvider) {
-        this.logDemoService = logDemoService;
-        this.myLoggerProvider = myLoggerProvider;
-    }
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
+
+        System.out.println("myLogger = " + myLogger.getClass());
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
