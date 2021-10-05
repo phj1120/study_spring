d1c15a3f810d101ce385b9702767401f91dbbd99
phj1120 <hyeonj1998@naver.com>
2021-10-05 21:21:08 +0900

    스코프와 Provider

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,8 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+//    private final MyLogger myLogger;
     /*
     스프링 컨테이너에 등록되고 받아오려 하는데 값이 없음
     얘는 생존 기간이 고객의 요청이 들어와서 나갈때 까지인데
@@ -26,15 +28,16 @@ public class LogDemoController {
     */
 
     @Autowired
-    public LogDemoController(LogDemoService logDemoService, MyLogger myLogger) {
+    public LogDemoController(LogDemoService logDemoService, ObjectProvider<MyLogger> myLoggerProvider) {
         this.logDemoService = logDemoService;
-        this.myLogger = myLogger;
+        this.myLoggerProvider = myLoggerProvider;
     }
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
