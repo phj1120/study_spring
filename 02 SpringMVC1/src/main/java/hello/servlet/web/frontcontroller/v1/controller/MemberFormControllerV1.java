97542e6ebed42f038476203a5c0b1367c3f5760c
phj1120 <hyeonj1998@naver.com>
2021-10-19 20:05:37 +0900

    view 분리 - v2

--- 02 SpringMVC1/src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberFormControllerV1.java
+++ 02 SpringMVC1/src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberFormControllerV1.java
@@ -13,6 +13,9 @@ public class MemberFormControllerV1 implements ControllerV1 {
     public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String viewPath = "/WEB-INF/views/new-form.jsp";
 //    컨트롤러에서 뷰로 이동할 때 사용하는 것
+//    RequestDispatcher
+//    클라이언트로부터 최초에 들어온 요청을 JSP/Servlet 내에서 원하는 자원으로 요청을 넘기는(보내는) 역할 수행
+//    특정 자원에 처리를 요청하고 처리 결과를 얻어오는 기능을 수행하는 클래스
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
     }
--- 02 SpringMVC1/src/main/java/hello/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
+++ 02 SpringMVC1/src/main/java/hello/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
@@ -1,8 +1,13 @@
-package hello.servlet.web.frontcontroller.v1;
+package hello.servlet.web.frontcontroller.v2;
 
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v1.ControllerV1;
 import hello.servlet.web.frontcontroller.v1.controller.MemberFormControllerV1;
 import hello.servlet.web.frontcontroller.v1.controller.MemberListControllerV1;
 import hello.servlet.web.frontcontroller.v1.controller.MemberSaveControllerV1;
+import hello.servlet.web.frontcontroller.v2.controller.MemberFormControllerV2;
+import hello.servlet.web.frontcontroller.v2.controller.MemberListControllerV2;
+import hello.servlet.web.frontcontroller.v2.controller.MemberSaveControllerV2;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,32 +19,30 @@ import java.util.HashMap;
 import java.util.Map;
 
 // 여기 들어오는 애는 일단 다 여기에 매핑 됨
-@WebServlet(name ="frontControllerServletV1", urlPatterns = "/front-controller/v1/*")
-public class FrontControllerServletV1 extends HttpServlet {
+@WebServlet(name ="frontControllerServletV2", urlPatterns = "/front-controller/v2/*")
+public class FrontControllerServletV2 extends HttpServlet {
 
-    private Map<String, ControllerV1> controllerMap = new HashMap<>();
+    private Map<String, ControllerV2> controllerMap = new HashMap<>();
 
 //    생성자.
 //    ServletApplication의 @ServletComponentScan에 등록될 때
 //    컨트롤러들을 FrontController 에 등록
-    public FrontControllerServletV1() {
-        controllerMap.put("/front-controller/v1/members/new-form", new MemberFormControllerV1());
-        controllerMap.put("/front-controller/v1/members/save", new MemberSaveControllerV1());
-        controllerMap.put("/front-controller/v1/members", new MemberListControllerV1());
+    public FrontControllerServletV2() {
+        controllerMap.put("/front-controller/v2/members/new-form", new MemberFormControllerV2());
+        controllerMap.put("/front-controller/v2/members/save", new MemberSaveControllerV2());
+        controllerMap.put("/front-controller/v2/members", new MemberListControllerV2());
 //        System.out.println(controllerMap);
     }
 
 //    HTTP 요청을 통해, 매핑된 URL이 호출되면 서블릿 컨테이너가 이 메서드 실행
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("FrontControllerServletV1.service");
-//        System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
 
 //        요청이 들어온 URI 확인
         String requestURI = request.getRequestURI();
 
 //        요청한  URI 에 맞는 컨트롤러 호출
-        ControllerV1 controller = controllerMap.get(requestURI);
+        ControllerV2 controller = controllerMap.get(requestURI);
 
 //        맞는 URI 가 없는 경우 404
         if (controller == null) {
@@ -49,6 +52,7 @@ public class FrontControllerServletV1 extends HttpServlet {
 
 //        해당 컨트롤러에 request, response 주고 호출
 //        이로 인해 각 컨트롤러를 서블릿으로 만들 필요도 없어짐(등록 할 필요 없음)
-        controller.process(request, response);
+        MyView view = controller.process(request, response);
+        view.render(request, response);
     }
 }
